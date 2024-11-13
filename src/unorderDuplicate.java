e2265179586301b0e7db377baa89bd06a6bdcec8
Maryam Ayomide Banjo <D00272896@student.dkit.ie>
2024-11-13 12:00:40 +0000

    Initial commit

---
+++ src/unorderDuplicate.java
@@ -0,0 +1,32 @@
+public class unorderDuplicate {
+    public static int[] duedupliate(int[] arr) {
+//       VALIDATION
+        if (arr == null) {
+            throw new IllegalArgumentException("Array is null");
+        }
+//            SETUP
+            int[] newArray = new int[arr.length];
+            int counter = 0;
+
+//            LOGIC
+            for (int i = 0; i < arr.length; i++) {
+
+            }
+
+        }
+ public static boolean contains (int [] array, int value, int pos){
+//        VALIDATION
+     if (array == null){
+         throw new IllegalArgumentException("Array cannot be null");
+     }
+     int count;
+     if ( count < 0 || count >= array.length ){
+         throw new IndexOutOfBoundsException("Count cannot be less than 0");
+     }
+//     LOGIC
+     for (int i = 0; i < count; i++) {
+         if
+
+     }
+ }
+    }
