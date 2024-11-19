893dd0b58471c0d2cd1d7701e63739c778ee1cf4
BabyGirlCodes <d0272896@student.dkit.ie>
2024-11-19 14:19:44 +0000

    unordered Duplicate using integers

--- src/unorderDuplicate.java
+++ src/unorderDuplicate.java
@@ -1,32 +1,60 @@
-public class unorderDuplicate {
-    public static int[] duedupliate(int[] arr) {
+public class unorderDuplicate
+{
+    public static int[] duedupliate(int[] array) {
 //       VALIDATION
-        if (arr == null) {
+        if (array == null) {
             throw new IllegalArgumentException("Array is null");
         }
 //            SETUP
-            int[] newArray = new int[arr.length];
-            int counter = 0;
+        int[] newArray = new int[array.length];
+        int count = 0;
 
 //            LOGIC
-            for (int i = 0; i < arr.length; i++) {
-
-            }
+        int value = 0;
+        for (int i = 0; i < array.length; i++) {
+         if(!contains(array, value,count)) {
+             newArray[count] = value;
+             count++;
+         }
 
         }
- public static boolean contains (int [] array, int value, int pos){
-//        VALIDATION
-     if (array == null){
-         throw new IllegalArgumentException("Array cannot be null");
-     }
-     int count;
-     if ( count < 0 || count >= array.length ){
-         throw new IndexOutOfBoundsException("Count cannot be less than 0");
-     }
-//     LOGIC
-     for (int i = 0; i < count; i++) {
-         if
-
-     }
- }
+        shrink(newArray,count);
+        return newArray;
     }
+
+    public static boolean contains(int[] array, int value, int pos) {
+//        VALIDATION
+        if (array == null) {
+            throw new IllegalArgumentException("Array cannot be null");
+        }
+        //int count = 0;
+        if ( pos >= array.length) {
+            throw new IndexOutOfBoundsException("Count cannot be less than 0");
+        }
+//     LOGIC
+        for (int i = 0; i < pos; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int [] shrink(int[] array, int count ){
+//        VALIDATION
+        if (array == null) {
+            throw new IllegalArgumentException("Array cannot be null");
+        }
+        if (count < 0 || count >= array.length) {
+            throw new IndexOutOfBoundsException("Count cannot be less than 0");
+        }
+
+//    SETUP
+        int [] newerArray = new int[count];
+
+        for (int i = 0; i < newerArray.length; i++) {
+            newerArray[i] = array[i];
+        }
+        return newerArray;
+    }
+}
+
